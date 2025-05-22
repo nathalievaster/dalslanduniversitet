83467736ab6742c854bf4e27a6d06f53e94c4bbe
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-22 09:44:05 +0200

    sidkomponenter och routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { MyCoursesComponent } from './pages/my-courses/my-courses.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent},
+    { path: 'courses', component: CoursesComponent},
+    { path: 'myCourses', component: MyCoursesComponent}
+];
