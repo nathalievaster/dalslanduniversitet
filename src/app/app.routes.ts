4634bd25f0980e0a5404b39cb34d3e605b9aa57c
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-22 10:53:48 +0200

    404-sida och övergripande style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CoursesComponent } from './pages/courses/courses.component';
 import { MyCoursesComponent } from './pages/my-courses/my-courses.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent},
     { path: 'courses', component: CoursesComponent},
     { path: 'myCourses', component: MyCoursesComponent},
-    { path: '', redirectTo: '/home', pathMatch: 'full'}
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: '404', component: NotFoundComponent}
 ];
