a979b571e4903f8bd70fd40c0c934b285590cbca
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-23 08:06:11 +0200

    service, interface för kurserna

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Course } from '../../models/course';
+import { CourseService } from '../../services/course.service';
 
 @Component({
   selector: 'app-courses',
@@ -7,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './courses.component.css'
 })
 export class CoursesComponent {
+  courselist: Course[] = [];
 
+  constructor(private courseservice : CourseService) {}
+
+  ngOnInit() {
+    this.courseservice.getCourses().subscribe(data => {
+      this.courselist = data;
+    })
+  }
 }
