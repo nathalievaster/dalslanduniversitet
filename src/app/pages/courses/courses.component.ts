2811a099ce00bf7fb30b698c3d8b50a322989308
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-23 15:45:14 +0200

    Sortering av tabeller

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -17,6 +17,36 @@ export class CoursesComponent {
   ngOnInit() {
     this.courseservice.getCourses().subscribe(data => {
       this.courselist = data;
-    })
+    });
   }
+  // Sorteringsvariabler
+sortField: string = '';
+sortAsc: boolean = true;
+
+sortCourses(field: string): void {
+  // Om användaren klickar på samma kolumn igen, växlar den riktning
+  if (this.sortField === field) {
+    this.sortAsc = !this.sortAsc; // växla riktning
+  } else {
+    // Vid klick på ny kolumn, börjar den om och sorterar den efter stigande ordning
+    this.sortField = field;
+    this.sortAsc = true;
+  }
+  // Sorteringen av listan
+  this.courselist.sort((a: any, b: any) => {
+    let valueA = a[field];
+    let valueB = b[field];
+
+    if (typeof valueA === 'string') {
+      // Ej skiftlägeskänsligt
+      valueA = valueA.toLowerCase();
+      valueB = valueB.toLowerCase();
+    }
+
+    if (valueA < valueB) return this.sortAsc ? -1 : 1;
+    if (valueA > valueB) return this.sortAsc ? 1 : -1;
+    return 0;
+  });
+}
+
 }
